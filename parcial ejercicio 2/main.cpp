1dd73e3592b279030d747443e11ff86424d930e3
LORDTRUPERD <168244173+LORDTRUPERD@users.noreply.github.com>
2025-03-08 10:56:26 -0600

    ejercicio 2.2
    ejercicio 2.2 subido por bryan adonias monroy leon carnet 0909 24 5252

---
+++ parcial ejercicio 2/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << " ingrese el 10 digitos";
+    int numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8, numero9,  numero10;
+    cin >> numero1;
+    cin >> numero2;
+    cin >> numero3;
+    cin >> numero4;
+    cin >> numero5;
+    cin >> numero6;
+    cin >> numero7;
+    cin >> numero8;
+    cin >> numero9;
+    cin >> numero10;
+    int  numerototal;
+    numerototal = numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10;
+ cout << numerototal << endl;
+    return 0;
+}
+
+
+
+
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
